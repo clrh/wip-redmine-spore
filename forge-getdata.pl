d8517f7a99db371491e5277c5a673076f734a67e
Claire Hernandez <claire.hernandez@biblibre.com>
2014-05-07 22:45:53 +0200

    test

--- forge-getdata.pl
+++ forge-getdata.pl
@@ -5,10 +5,20 @@ use Net::HTTP::Spore;
 use Data::Dumper;
 use Config::YAML;
 
-my $conf = Config::YAML->new( config => "myforge.yaml");
 
-my $client = Net::HTTP::Spore->new_from_spec('redmine.json',base_url => $conf->get_base_url);
+my $conf = Config::YAML->new( config => "myforge.yaml");
+my $client = Net::HTTP::Spore->new_from_spec(
+	'redmine.json',
+	trace => "1=log.txt",
+	base_url => $conf->get_base_url
+);
+
+$client->enable(
+	'Auth::Basic',
+	username => $conf->get_user,
+	password => $conf->get_pass
+);
 $client->enable('Format::JSON');
 
-say $client->get_issue(42,'json');
-
+my $issue =  $client->get_issue(1042,'json');
+say Data::Dumper::Dumper $issue;
