a9a11ab2eb4f1455c8dafb328b7b287dfb5e6f0c
Claire Hernandez <claire.hernandez@biblibre.com>
2014-05-09 14:21:56 +0200

    tmp

--- forge-getdata.pl
+++ forge-getdata.pl
@@ -13,12 +13,22 @@ my $client = Net::HTTP::Spore->new_from_spec(
 	base_url => $conf->get_base_url
 );
 
-$client->enable(
-	'Auth::Basic',
-	username => $conf->get_user,
-	password => $conf->get_pass
-);
+#$client->enable(
+#	'Auth::Basic',
+#	username => $conf->get_user,
+#	password => $conf->get_pass
+#);
+#warn "user:".$conf->get_user.";pass:".$conf->get_pass.";url:".$conf->get_base_url;
 $client->enable('Format::JSON');
 
 my $issue =  $client->get_issue(1042,'json');
 say Data::Dumper::Dumper $issue;
+
+#say 'coucou';
+#my $response = $client->get_issue(id => '12808',format => 'json');
+#my $issues = $response->{issue};
+#say Data::Dumper::Dumper($response);
+#
+#foreach my $issue (@$issues) {
+#  say $issue->{id};
+#}
