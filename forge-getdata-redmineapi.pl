e191788a5d5408072c4c70401f39c79662b15085
Claire Hernandez <claire.hernandez@biblibre.com>
2014-05-09 23:39:26 +0200

    note

--- forge-getdata-redmineapi.pl
+++ forge-getdata-redmineapi.pl
@@ -19,23 +19,6 @@ my $client = Redmine::API->new(
 #say dump $client->queries->list->all;
 #say Data::Dumper::Dumper $client->issues->list->all(query_id => '496',project_id=>'56');
 
-
-
-use JSON;
-my $filename = 'issues.mock.json';
-
-my $json_text = do {
-   open(my $json_fh, "<:encoding(UTF-8)", $filename)
-      or die("Can't open \$filename\": $!\n");
-   local $/;
-   <$json_fh>
-};
-
-my $json = JSON->new;
-my $issues = $json->decode($json_text);
-
-for ( @{$issues->{issues}} ) {
-   print $_->{id}."\n";
-}
-
+#&limit=200
+#offset=?
 
