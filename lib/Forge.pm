e8cfa541a3436cbfc32fb786f5874df5686f0757
Claire Hernandez <claire.hernandez@biblibre.com>
2014-05-11 21:38:40 +0200

    better module

--- lib/Forge.pm
+++ lib/Forge.pm
@@ -7,28 +7,53 @@ use warnings;
 require Exporter;
 
 our @ISA = qw(Exporter);
-
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use Forge ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
 our %EXPORT_TAGS = ( 'all' => [ qw(
-	
+ count_issues
+ count_issues_by_status
 ) ] );
 
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 
 our @EXPORT = qw(
-	
+  count_issues
+  count_by_status
 );
 
 our $VERSION = '0.01';
 
+sub count_issues {
+  my $issues = shift;
+  my @ids;
+  my $issue;
+  foreach $issue ( @{$issues->{issues}} ) {
+    push @ids, $issue->{id};
+  }
+  $#ids + 1;
+}
 
-# Preloaded methods go here.
+sub count_by_status {
+  my $issues = shift;
+  my @lists = (
+    {id=>1, name=>"A qualifier"},
+    {id=>14, name=>"A planifier"},
+    {id=>7, name=>"En développement"},
+    {id=>3, name=>"Réalisé à tester"}
+  );
+  my $aqual;my $aplan; my $endev; my $atest;
+  foreach my $issue ( @{$issues->{issues}} ) {
+	  my $status_id = $issue->{status}->{id};
+          $aqual += 1 if ($status_id eq 1);
+          $aplan += 1 if ($status_id eq 14);
+          $endev += 1 if ($status_id eq 7);
+          $atest += 1 if ($status_id eq 3);
+  }
+  my %count_issues_by_status = (
+    1  => $aqual,
+    14 => $aplan,
+    7  => $endev,
+    3  => $atest,
+  );
+}
 
 1;
 __END__
--- t/Forge.t
+++ t/Forge.t
@@ -8,11 +8,35 @@
 use strict;
 use warnings;
 
-use Test::More tests => 1;
+use Test::More;
+use JSON;
+use Data::Dump qw/dump/;
+use Data::Dumper qw/Dumper/;
+use lib "lib";
+
 BEGIN { use_ok('Forge') };
 
-#########################
+sub read_file_and_decode {
+  my $filename = shift;
+  my $json_text = do {
+     open(my $json_fh, "<:encoding(UTF-8)", $filename)
+        or die("Can't open \$filename\": $!\n");
+     local $/;
+     <$json_fh>
+  };
 
-# Insert your test code below, the Test::More module is use()ed here so read
-# its man page ( perldoc Test::More ) for help writing this test script.
+  my $json = JSON->new;
+  $json->decode($json_text);
+}
 
+my $filename = './t/fixtures/issues.mock.json';
+my $issues = read_file_and_decode $filename;
+is (count_issues($issues),25,'number of issues');
+
+$filename = './t/fixtures/issues-full.mock.json';
+$issues = read_file_and_decode $filename;
+is (count_issues($issues),58,'number of issues');
+my %sorted_issues = count_by_status($issues);
+is ($sorted_issues{7},3,'number of -en dev- issues');
+
+done_testing;
